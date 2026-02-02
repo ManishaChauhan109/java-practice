3abeafb4df8fd8fbe1d7461488f5a415fcd0f099
ManishaChauhan109 <manishavns109@gmail.com>
2026-02-02 12:20:17 +0530

    Initial commit

---
+++ Fabonnaci_DP.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+//Fabonacci series using DP(memoization method)
+class Fabonnaci_DP{
+    static int fabonacci(int n,int[] dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = fabonacci(n-1,dp) + fabonacci(n-2,dp);
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 10; // Example input
+         int[] dp = new int[n+1];
+         Arrays.fill(dp,-1);
+        System.out.println("Fabonacci of " + n + " is: " + fabonacci(n,dp));
+    } 
+}
